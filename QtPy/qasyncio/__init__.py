abd25a0f8d68a176bb591d20541ceffe3ece6ede
Brian Cefali <brian@atakama.com>
2022-01-27 21:51:06 -0500

    Catch import errors for event loop impls

--- QtPy/qasyncio/__init__.py
+++ QtPy/qasyncio/__init__.py
@@ -1,3 +1,13 @@
-from QtPy.qasyncio._env import QtEventLoop, QtProactorEventLoop, QtSelectorEventLoop
+from QtPy.qasyncio._env import QtEventLoop
+
+try:
+    from QtPy.qasyncio._env import QtProactorEventLoop
+except ImportError:
+    QtProactorEventLoop = None
+
+try:
+    from QtPy.qasyncio._env import QtSelectorEventLoop
+except ImportError:
+    QtSelectorEventLoop = None
 
 __all__ = ["QtEventLoop", "QtProactorEventLoop", "QtSelectorEventLoop"]
